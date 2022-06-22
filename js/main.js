89d886278d5bdfee9a02594cbee7ae858ce2eab8
mpaszkeiwicz <m.paszkiewicz@i-propert.pl>
2022-06-22 11:50:22 +0200

    add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 let sotrString = 'Akademia108';
-let str = sotrString.split('')
-str.sort();
-console.log(str);
+// let str = sotrString.split('')
+// str.sort();
+// console.log(str);
 
+function odwrocString(napis) {
+    let strToArray = napis.split(""); //ad2
+    let arraySort = strToArray.sort(); //ad3
+    let arrayToString = arraySort.join(""); //ad4
+
+    return arrayToString; //ad5
+}
+
+let odwrocnyNapis = odwrocString(sotrString) // ad1
+console.log(odwrocnyNapis);
+
+console.log(odwrocString("marek"));
+console.log(odwrocString("Krystian"));let
+
+// 1. przekazać string o funkcji
+// 2. zamienić string na tablicę
+// 3. posortować tablicę
+// 4. zamienić posortowaną tablicę na  stringa
+// 5. zwrócić posprtowanego stringa
